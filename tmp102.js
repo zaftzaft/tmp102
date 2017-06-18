d9d2efbbce293d92c74337e39847a39827e2a7e5
Aoki <zaftzaft7@gmail.com>
2017-06-18 17:24:18 +0900

    update

--- tmp102.js
+++ tmp102.js
@@ -1,18 +1,29 @@
 'use strict';
 
 const exec = require("child_process").execSync;
-const fs = require("fs");
+const fs   = require("fs");
+const path = require("path");
+
+let filename = process.argv[2] || "/tmp/node_exporter/tmp102.prom";
 
 let val = ""+exec("i2cget -y 1 0x48 0x00 w");
 
-let lsb = val.slice(2,3);
-let msb = val.slice(4,6);
+let lsb = val.slice(2, 3);
+let msb = val.slice(4, 6);
 
 let dec = (parseInt(`0x${msb}${lsb}`, 16) * 0.0625).toFixed(4);
 
 if(dec.slice(-4) === "0000") {
-    dec = `${dec.slice(0, -1)}1`;
+  dec = `${dec.slice(0, -1)}1`;
 }
 
-fs.writeFileSync("/opt/node_exporter_directory/tmp102.prom.$$", `sensors_temp ${dec}\n`, "utf8");
-fs.renameSync("/opt/node_exporter_directory/tmp102.prom.$$", "/opt/node_exporter_directory/tmp102.prom");
+if(process.argv[3] == "1" || process.argv[2] === void 0) {
+  try{
+    fs.mkdirSync(path.dirname(filename));
+  }catch(e){
+    //if(e.errno === -17) {}
+  }
+}
+
+fs.writeFileSync(`${filename}.$$`, `sensors_temp ${dec}\n`, "utf8");
+fs.renameSync(`${filename}.$$`, filename);
